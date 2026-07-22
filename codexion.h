54e466fcafff7408ba7aa9fcf93214d2b6a3b19d
Nataniel Appasamy <nappasam@k2r2p9.42mulhouse.fr>
2026-07-22 19:55:24 +0200

    jsp

--- codexion.h
+++ codexion.h
@@ -2,6 +2,7 @@
 # define CODEXION_H
 
 
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,22 +12,62 @@
 
 typedef enum e_sched
 {
-    FIFO,
-    EDF
-}   t_sched;
+	FIFO,
+	EDF
+}	t_sched;
+
+typedef enum e_dongle_state
+{
+	DONGLE_FREE,
+	DONGLE_HELD,
+	DONGLE_COOLDOWN
+}	t_dongle_state;
 
 typedef struct s_config
 {
-    int number_of_coder;
-    int time_to_burnout;
-    int time_to_compile;
-    int time_to_debug;
-    int time_to_refactor;
-    int number_of_compiles_required;
-    int dongle_cooldown;
-    t_sched mode;
-}              t_config;
+	int		number_of_coder;
+	int		time_to_burnout;
+	int		time_to_compile;
+	int		time_to_debug;
+	int		time_to_refactor;
+	int		number_of_compiles_required;
+	int		dongle_cooldown;
+	t_sched	mode;
+}	t_config;
 
+typedef struct s_table	t_table;
+
+typedef struct s_dongle
+{
+	t_dongle_state	state;
+	long			released_at;
+	pthread_mutex_t	lock;
+	pthread_cond_t	cond;
+	void			*waiting;
+}	t_dongle;
+
+typedef struct s_coder
+{
+	int				number;
+	long			last_compile_start;
+	int				compile_counter;
+	int				left_dongle;
+	int				right_dongle;
+	pthread_mutex_t	compile_lock;
+	t_table			*table;
+}	t_coder;
+
+typedef struct s_table
+{
+	t_config		*config;
+	long			start_time;
+	t_coder			*coders;
+	t_dongle		*dongles;
+	int				is_over;
+	pthread_mutex_t	print_lock;
+	pthread_mutex_t	table_lock;
+}	t_table;              t_dongle;
 
 t_config parser(int ac, char **av);
+void	error_exit(void);
 #endif
--- main.c
+++ main.c
@@ -9,21 +9,67 @@ static const char	*mode_str(t_sched mode)
 		return ("fifo");
 	return ("edf");
 }
+long	get_time_ms(void)
+{
+	struct timeval	tv;
+
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
+}
+
+void	precise_sleep(long ms)
+{
+	long	start;
+
+	start = get_time_ms();
+	while ((get_time_ms() - start) < ms)
+		usleep(400);
+}
+
+
+void	error_exit_free(void *any)
+{
+	write(2, "Error\n", 6);
+    free(any);
+	exit(1);
+}
 
 int	main(int ac, char **av)
 {
 	t_config	config;
+    t_table		table;
 
-	config = parser(ac, av);
-	printf("--- parse OK ---\n");
-	printf("number_of_coder            = %d\n", config.number_of_coder);
-	printf("time_to_burnout            = %d\n", config.time_to_burnout);
-	printf("time_to_compile            = %d\n", config.time_to_compile);
-	printf("time_to_debug              = %d\n", config.time_to_debug);
-	printf("time_to_refactor           = %d\n", config.time_to_refactor);
-	printf("number_of_compiles_required = %d\n",
-		config.number_of_compiles_required);
-	printf("dongle_cooldown            = %d\n", config.dongle_cooldown);
-	printf("mode                       = %s\n", mode_str(config.mode));
+    memset(&table, 0, sizeof(t_table));
+    config = parser(ac, av);
+    table.config = &config;
+    table.coders = malloc(config.number_of_coder * sizeof(t_coder));
+    if (!table.coders)
+        error_exit();
+    table.dongles = malloc(config.number_of_coder * sizeof(t_dongle));
+    if (!table.dongles)
+        error_exit_free(table.coders);
+	// printf("--- parse OK ---\n");
+	// printf("number_of_coder            = %d\n", config.number_of_coder);
+	// printf("time_to_burnout            = %d\n", config.time_to_burnout);
+	// printf("time_to_compile            = %d\n", config.time_to_compile);
+	// printf("time_to_debug              = %d\n", config.time_to_debug);
+	// printf("time_to_refactor           = %d\n", config.time_to_refactor);
+	// printf("number_of_compiles_required = %d\n",
+	// 	config.number_of_compiles_required);
+	// printf("dongle_cooldown            = %d\n", config.dongle_cooldown);
+	// printf("mode                       = %s\n", mode_str(config.mode));
+    // printf("time testing \n");
+    // struct timeval	tv;
+
+	// gettimeofday(&tv, NULL);
+	// printf("seconds:      %ld\n", tv.tv_sec);
+	// printf("microseconds: %ld\n", tv.tv_usec);
+    // long start = get_time_ms();
+    // printf("%ld\n",start);
+    // precise_sleep(200);
+    // long end = get_time_ms();
+    // long elapsed = end - start;
+    // printf("%ld\n", elapsed);
+
 	return (0);
 }
